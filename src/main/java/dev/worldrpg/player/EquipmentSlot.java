88fb1f86acb1dbeef1d37e6317011a908bdd46f2
VickMASpc <vcmas09@gmail.com>
2026-09-26 18:20:35 -0300

    Add EquipmentSlot.java

---
+++ src/main/java/dev/worldrpg/player/EquipmentSlot.java
@@ -0,0 +1,14 @@
+package dev.worldrpg.player;
+
+public enum EquipmentSlot {
+    HEAD,
+    CHEST,
+    HANDS,
+    LEGS,
+    FEET,
+    BACK,
+    MAIN_HAND,
+    OFF_HAND,
+    NECK,
+    TRINKET
+}
